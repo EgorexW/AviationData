73b27d2a7cb879983904fe4443b9501fbef61b3f
EgorexW <olko.wiktor.10@gmail.com>
2024-06-23 19:03:41 +0200

    Using Resharp Json Deserializing

--- Staż/AviationDataController.cs
+++ Staż/AviationDataController.cs
@@ -1,4 +1,7 @@
+using System.Diagnostics;
+using RestSharp;
 using System.Text.Json;
+using System.Text.Json.Nodes;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Staż.Controllers;
@@ -7,36 +10,32 @@ namespace Staż.Controllers;
 [Route("[controller]")]
 public class AviationDataController : ControllerBase
 {
-    static HttpClient sharedClient = new();
+    HttpClient sharedClient = new();
+
     [HttpGet]
     public async Task<string> GetFlightsInTheAir(string departureCode, string arrivalCode)
     {
         var output = "";
         var request = await GetRequest(departureCode, arrivalCode);
-        var document = JsonDocument.Parse(request);
-        var data = document.RootElement.GetProperty("data");
-        if (data.GetArrayLength() == 0){
-            output = "No flights in the air";
-        }
-        else{
-            var arrayEnumerator = data.EnumerateArray();
-            foreach (var flight in arrayEnumerator){
-                var flightText = "\nFlight: nr: " + flight.GetProperty("flight").GetProperty("number").GetString() + " airline: " + flight.GetProperty("airline").GetProperty("name").GetString() + " estimated time of arrival: " + flight.GetProperty("arrival").GetProperty("estimated").GetString() + "\n";
-                output += flightText;
-            }
+        // Use restsharp
+        var data = JsonSerializer.Deserialize<JsonNode>(request);
+        Debug.Assert(data != null, nameof(data) + " != null");
+        foreach (var flight in data["data"]!.AsArray()){
+            var flightText = $"Flight: nr: {flight!["flight"]!["number"]} airline: {flight["airline"]!["name"]} estimated time of arrival: {flight["arrival"]!["estimated"]}\n";
+            output += flightText;
         }
         return output;
     }
 
-    static async Task<string> GetRequest(string departure, string arrival)
+    async Task<string> GetRequest(string departure, string arrival)
     {
-        var requestUri = "http://api.aviationstack.com/v1/flights?access_key=" + YOUR_ACCESS_KEY + "&dep_iata=" + departure + "&arr_iata=" + arrival + "&flight_status=active";
-        Console.WriteLine("Request URI: " + requestUri);
+        var requestUri = $"http://api.aviationstack.com/v1/flights?access_key={YOUR_ACCESS_KEY}&dep_iata={departure}&arr_iata{arrival}&flight_status=active";
+        Console.WriteLine("Request URI: " + requestUri); //replace with serilog
         Console.WriteLine("Request sent");
         var response = await sharedClient.GetStringAsync(requestUri);
         Console.WriteLine("Response received");
         return response;
     }
 
-    const string YOUR_ACCESS_KEY = "656d03ab8936ca69c67727f8cfc8a299";
+    const string YOUR_ACCESS_KEY = "656d03ab8936ca69c67727f8cfc8a299"; //Move to appsettings.json
 }
